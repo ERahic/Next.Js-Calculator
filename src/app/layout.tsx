33d5fb81c1278c32d2b96259f40b9b05b834ea3d
Edis Rahic <rahic.edis@gmail.com>
2024-05-29 18:47:22 -0400

    Continuous equation can now be done before pressing equals key

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-const name = "Edis Rahic"
+const name = "Edis Rahic";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,157 @@
+import "../app/globals.css";
+import React, { useState } from "react";
+import { evaluate } from "mathjs";
+import Wrapper from "../components/Wrapper";
+import Screen from "../components/Screen";
+import ButtonBox from "../components/ButtonBox";
+import Button from "../components/Button";
+
+export default function Home() {
+  // useState to store memory of operands entered by user
+  const [currentNumber, setCurrentNumber] = useState<string>("0");
+  const [priorNumber, setPriorNumber] = useState<string>("");
+  const [sign, setSign] = useState<string>("");
+  const [solution, setSolution] = useState<string>("");
+  // Array of buttons that will be set on each button via .map()
+  const buttonValues = [
+    "C",
+    "+-",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "x",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "0",
+    ".",
+    "=",
+  ];
+
+  // Function for when a button is pressed and to have a case for each button
+  const processKeystroke = (key: string) => {
+    switch (key) {
+      // Case to clear screen and useState of any variables saved
+      case "C":
+        setCurrentNumber("0");
+        setPriorNumber("0");
+        setSign("");
+        break;
+      // case for number button keys
+      case "0":
+        // Stop adding 0 if first character of current number is 0
+        if (currentNumber === "0") {
+          break;
+        }
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        // Calculator screen display limit
+        if (currentNumber.length >= 20) {
+          break;
+        }
+        // Change 0 to key entered
+        if (currentNumber === "0") {
+          setCurrentNumber(key);
+          break;
+        }
+        // Add new number keys onto current number
+        setCurrentNumber((x) => {
+          return x + key;
+        });
+        break;
+      // Case for converting current number to negative/positive
+      case "+-":
+        if (currentNumber === "0") {
+          break;
+        } else if (currentNumber.includes("-")) {
+          setCurrentNumber((x) => {
+            return x.replace("-", "");
+          });
+        } else {
+          setCurrentNumber((x) => {
+            return `${"-" + x}`;
+          });
+        }
+        break;
+      // Case for decimal key
+      case ".":
+        if (currentNumber.includes(".")) {
+          return;
+        } else {
+          setCurrentNumber((x) => {
+            return x + key;
+          });
+        }
+        break;
+      // Case for signs
+      case "%":
+      case "+":
+      case "-":
+      case "x":
+      case "/":
+        if (key === "x") {
+          setSign("*");
+          setPriorNumber(currentNumber);
+          setCurrentNumber("0");
+          break;
+        }
+        if (priorNumber && key === "/" && currentNumber === "0") {
+          setCurrentNumber("Error, Cannot Divide By 0");
+          break;
+        }
+        setSign(key);
+        setPriorNumber(currentNumber);
+        setCurrentNumber("0");
+        break;
+      // Case for when evaluating given equation
+      case "=":
+        if (priorNumber && sign === "/" && currentNumber === "0") {
+          setCurrentNumber("Error, Cannot Divide By 0");
+          break;
+        }
+        setCurrentNumber(`${evaluate(priorNumber + sign + currentNumber)}`);
+        setSolution(`${evaluate(priorNumber + sign + currentNumber)}`);
+    }
+  };
+
+  return (
+    <>
+      <div className="w-flex md:w-auto">
+        <p>
+          Hello! My name is Edis Rahic and thank you for checking out my first
+          project using Next.js React. Please give this a try to do your
+          homework or taxes, it works!
+        </p>
+        <Wrapper>
+          <Screen className="screen" value={currentNumber}></Screen>
+          <ButtonBox className="buttonBox">
+            {buttonValues.map((btn, i) => {
+              return (
+                <Button
+                  key={i}
+                  className={`button ${btn === "=" ? "equals" : ""}`}
+                  value={btn}
+                  onClick={processKeystroke}
+                ></Button>
+              );
+            })}
+          </ButtonBox>
+        </Wrapper>
+      </div>
+    </>
+  );
+}
